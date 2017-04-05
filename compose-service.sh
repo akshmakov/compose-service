58cce5b10cb1806fda09469878aa0775afcc0167
Andrey K Shmakov <akshmakov@gmail.com>
2017-04-05 00:56:52 -0700

    First Test run of init_d Deployment Succeed - finished initial feature implementation

--- compose-service.sh
+++ compose-service.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-#Common Things
-### Usage
-### Command Line options
-### compose-service install
+### compose-service
+###  a tool to manage docker-compose deployments as services
+###  on linux systems
 ###
-### Environment Variables
+### A service is roughly speaking a "permanent" docker-compose deployment
+###
+###
+### Services can be deployed under:
+###  crontab - schedule service start on power on via crontab
+###  init.d  - register service as its own init.d script
+###  compose-service - register service under the generic compose-service init.d
+###  
+###
+### Environment Variables can override defaults
 ### COMPOSE_SERVICE_TARGET_DIR -t --target-dir
 ###   Default: /var/opt/compose-service/deploy/
 ### COMPOSE_SERVICE_CONFIG_DIR -c --config-dir
@@ -26,21 +34,17 @@
 ###       Boiler Plate Stuff ############################
 #########################################################
 
-## Service Global Variables ##
+## compose-service utility Global Variables ##
 PROGNAME=$(basename $0)
 INVOKE_TS=$(date +"%s")
-
-
-COMMAND_LIST="install install-service install-cron update remove destroy deploy"
-
 
 
 function error_exit
 {
     #----------------------------------------------------------------
-    #Function for exit due to fatal program error
-    #Accepts 1 argument:
-    #string containing descriptive error message
+    # Function for exit due to fatal program error
+    # Accepts 1 argument:
+    # string containing descriptive error message
     #----------------------------------------------------------------
     echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
     exit 1
@@ -48,6 +52,11 @@ function error_exit
 
 function run_dry
 {
+    #-----------------------------------------------------
+    # If DRY_RUN != NO in Environment,
+    #  simply echo the command
+    # Limitation: Does not handle pipes/redirects
+    #-----------------------------------------------------
     if [[ $DRY_RUN = NO ]]; then
 	${@}
     else
@@ -87,14 +96,27 @@ EOF
 ######## Service Variables ########################
 ###################################################
 
+# Service Specific Variable File (analog to docker-compose.yml)
 SERVICE_FILE=${COMPOSE_SERVICE_DEFINITION-$(pwd)/compose-service}
+
+# Default Service Name (typically set by service file)
 SERVICE_NAME=${COMPOSE_SERVICE_NAME-"compose-service-$INVOKE_TS"}
+
+# Service YML deployment root dir
 TARGET_DIR=${COMPOSE_SERVICE_TARGET_DIR-/var/opt/compose-service/deploy}
+
+# Service YML Data Volume Root
 DATA_DIR=${COMPOSE_SERVICE_DATA_DIR-/var/opt/compose-service}
+
+# Configuration Directory
 CONF_DIR=${COMPOSE_SERVICE_CONFIG_DIR-/etc/compose-service}
+
+###Application Flags ###
 VERBOSE=0
 FORCE=NO
 DRY_RUN=NO
+
+### Command Placeholder
 COMMAND="HELP"
 
 
@@ -102,6 +124,10 @@ COMMAND="HELP"
 ########## Options Processing #######################
 #####################################################
 
+## Following Parses the -abc/--do-thing-xyz options
+## Most options modify one of the service variables above
+## A few set Application Flags
+## Command Processing Happens After
 
 if [[ $# = 0 ]]; then
     usage
@@ -158,6 +184,10 @@ do
     esac
 done
 
+
+## All options have "-" or "--"
+## First string after options is our command
+## Command Arguments can be added later
 if [[ -n $1 ]]; then
     COMMAND=$1
     echo "Command: $COMMAND"
@@ -167,7 +197,16 @@ fi
 
 
 		  
-#setup verbose mode
+### setup verbose mode
+# Nifty Trick found on SO
+# You can write your own verbose only
+# log string by repacing your
+# echo "text string here"
+# with
+# echo "text string here" >&3
+# for stdout (verbose)
+# and >&4 for stderr (verbose)
+###
 if [ "$VERBOSE" = 1 ]; then
     exec 4>&2 3>&1
 else
@@ -175,9 +214,19 @@ else
 fi
 
 
-if [[ $DRY_RUN == "NO" &&  $EUID = 0 ]]; then
-    error_exit "Please run as root"
+
+### Check Basic Permissions 
+if [[ $DRY_RUN == "NO" \
+	    && ! -r $TARGET_DIR \
+	    && ! -r $DATA_DIR \
+	    && ! -r $CONF_DIR \
+    ]];
+then
+    
+    error_exit "Missing Required Dirs or No Read Access  $TARGET_DIR, $DATA_DIR, or  $CONF_DIR"
+    
 fi
+
 
 
 
@@ -204,6 +253,14 @@ EOF
 #### Load  Service Specific Environment ########################
 ################################################################
 
+###
+### We define a "shell environment api"
+### that we source from the service file
+### a docker-compose style override is also checked
+###
+### We check for some basic required variables 
+###
+
 if [ -e  $SERVICE_FILE ] ; then
     source $SERVICE_FILE
 fi
@@ -224,12 +281,20 @@ fi
 
     
 
-
+# our docker-compose yml file for service deployments
 SERVICE_DEPLOY_YML=${SERVICE_DEPLOY_YML-"./docker-service.yml"}
+
+# the target path/name of the compose yml
 SERVICE_DEPLOYMENT=$TARGET_DIR/$SERVICE_NAME.yml
+
+# service specific data directory
 SERVICE_DATA=$DATA_DIR/$SERVICE_NAME
+
+# Service configurations live in subdir of CONF_DIR
 SERVICE_CONFIG_ROOT=$CONF_DIR/service.d
 SERVICE_CONFIG=$SERVICE_CONFIG_ROOT/$SERVICE_NAME.service
+
+
 
 cat <<EOF >&3
 ---------------------debug-------------------------
@@ -243,11 +308,47 @@ EOF
 
 
 ##########################################################
-### Deployment Functions ####### #########################
+### Deployment Functions #################################
 ##########################################################
+
+
+#
+# Each command supported has an associated function
+#   do_command
+#
+# and possibly a few helper functions
+# the following is organized by major command
+#
+# deploy
+# install-initd
+# install-cron
+# install-compose
+# update
+# remove
+# destroy
+#
+
+
+######
+###### DO_DEPLOY
+######
+
 function prep_deployment
 {
-    
+    #----------------------------------------
+    # Check for old content and create service folders
+    # if necessary
+    #
+    # Outcome - all primary compose-service
+    # folders - config, target, data
+    # exist and are clean to be used
+    # this function may fail in circumstances
+    # when we should not be deploying , e.g.
+    # if certain files exist when we try
+    # to deploy (and arent using -f/--force)
+    #----------------------------------------
+
+    #likely never gets used
     if [[ ! -d $CONF_DIR && $FORCE != YES ]]; then
 	error_exit "Cannot Deploy $SERVICE_NAME, Configuration Directory $CONF_DIR doesn't exist or isn't a directory"
     fi
@@ -261,26 +362,36 @@ function prep_deployment
 	fi
     fi
 
+
+    #------------
+    # Check Data Directory
+    #------------
     if [[ ! -d $DATA_DIR ]]; then
 	error_exit "Cannot Deploy $SERVICE_NAME, Data Root Directory $DATA_DIR doesn't exist or isn't a directory"
     fi
 
+    #------------
+    # Check for Target Dir
+    #------------
     if [[ ! -d $TARGET_DIR ]]; then
 	error_exit "Cannot Deploy $SERVICE_NAME, Target Dir $TARGET_DIR doesn't exist"
     fi
 
 
+    #------------
+    # Create our service root
+    #------------
     if [[ -d $SERVICE_DATA ]]; then
-	echo "-debug- Service Data Folder $SERVICE_DATA exists"
+	echo "-debug- Service Data Folder $SERVICE_DATA exists" >&3
     else
-	if [[ $DRY_RUN = NO ]]; then
-	    mkdir -p $SERVICE_DATA
-	else
-	    echo "-dry-run- mkdir -p $SERVICE_DATA"
-	fi
+	run_dry mkdir -p $SERVICE_DATA
     fi
 
 
+    #-----------
+    # Check for pre-existing config/deployment files
+    # fail if they exist and not forced
+    #-----------
     
     if [[ -e $SERVICE_DEPLOYMENT && $FORCE != YES ]]; then
 	error_exit "Cannot Deploy $SERVICE_NAME Deployment : $SERVICE_DEPLOYMENT exists  - use update or --force"
@@ -326,17 +437,6 @@ EOF
     return 0
 }
 
-function deploy_service
-{
-    return 0
-}
-
-function destroy_service
-{
-    return 0
-}
-
-
 function do_deploy
 {
     prep_deployment
@@ -375,25 +475,197 @@ EOF
 
 }
 
-function do_install_initd
+
+
+#######
+####### do_install_initd
+#######
+
+### Initd Locals
+INITD_INSTALL_DIR=/etc/init.d
+SERVICE_INITD_INSTALL=$INITD_INSTALL_DIR/$SERVICE_NAME
+
+# Deploy init_d service files
+function prep_initd_service
+{
+    
+    if [[ -e $SERVICE_INITD_INSTALL ]]; then
+	error_exit "init_d service for $SERVICE_NAME already exists"
+    fi
+
+
+    # po'mans templating
+    
+    initd_data=$(cat <<-EOF
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:		$SERVICE_NAME
+# Required-Start:	\$docker
+# Required-Stop:	\$docker
+# Default-Start:	2 3 4 5
+# Default-Stop:		0 1 6
+# Short-Description:	compose-service for $SERVICE_NAME
+### END INIT INFO
+
+set -e
+
+SERVICE_NAME=$SERVICE_NAME
+SERVICE_CONFIG=$SERVICE_CONFIG
+SERVICE_DATA=$SERVICE_DATA
+SERVICE_DEPLOYMENT=$SERVICE_DEPLOYMENT
+
+OPTS="-f \$SERVICE_DEPLOYMENT -p \$SERVICE_NAME "
+
+UPOPTS="-d --no-recreate --no-build --no-deps"
+ 
+. /lib/lsb/init-functions
+
+case "\$1" in
+    start)
+        log_daemon_msg "Starting compose-service" "\$SERVICE_NAME" ||  true
+        docker-compose \$OPTS up \$UPOPTS
+        ;;
+
+    stop)
+        log_daemon_msg "Stopping compose-service" "\$SERVICE_NAME" || true
+        docker-compose \$OPTS stop
+        ;;
+
+    reload)
+        log_daemon_msg "Reloading compose-service" "\$SERVICE_NAME" || true
+        docker-compose \$OPTS up \$UPOPTS
+        ;;
+
+    restart)
+        log_daemon_msg "Restarting compose-service" "\$SERVICE_NAME" || true
+        docker-compose \$OPTS stop
+        docker-compose \$OPTS up \$UPOPTS
+        ;;
+
+    recreate)
+        log_daemon_msg "Recreating compose-service" "\$SERVICE_NAME" || true
+        docker-compose \$OPTS down
+        docker-compose \$OPTS up \$UPOPTS
+        ;;
+
+    *)
+        log_action_msg "Usage: /etc/init.d/composegogs {start|stop|restart|reload|recreate}" || true
+        exit 1
+        ;;
+esac
+
+exit 0
+EOF
+		)
+
+
+    # repeated cats but who cares I like cats
+    
+    cat <<-EOF >&3
+--------debug-------$SERVICE_NAME---initd-data----------
+$initd_data
+----------------------------------------------------------
+EOF
+
+    
+    if [[ $DRY_RUN = NO ]]; then
+	cat <<-EOF > $SERVICE_INITD_INSTALL
+$initd_data
+EOF
+    else
+	cat <<-EOF
+--dry-run---cat > $SERVICE_INITD_INSTALL
+$initd_data
+EOF
+    fi
+
+    run_dry chmod +x $SERVICE_INITD_INSTALL
+    
+    #update-rc.d $SERVICE_NAME defaults
+    
+    return 0
+}
+
+
+function destroy_initd_service
 {
     return 0
 }
+
+
+
+function do_install_initd
+{
+    
+    if [[ $DRY_RUN == "NO" \
+		&& -w $SERVICE_INITD_INSTALL_DIR \
+	]]; then
+	error_exit "Run initd install with sudo pleaseWrite Access to /etc/init.d to install service"
+    fi
+
+    prep_initd_service
+    
+
+    
+    return 0
+}
+
+
+
+
+
+
+
+
+#######
+####### do_install_cron
+#######
+
+
+
+
 
 function do_install_cron
 {
     return 0
 }
 
+
+
+
+
+
+
+
+#######
+####### do_install_compose
+#######
+
+
 function do_install_compose
 {
     return 0
 }
 
+
+
 function do_update
 {
     return 0
 }
+
+
+
+
+
+
+
+
+#######
+####### do_remove
+#######
+
+
 
 function do_remove
 {
@@ -415,6 +687,17 @@ function do_remove
     
     return 0
 }
+
+
+
+
+
+
+#######
+####### do_destroy
+#######
+
+
 
 function do_destroy
 {
